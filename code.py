b287a87ebee3080798b9d23eeadd15d5168a0515
Kevin O'Connor <Kevin@MacBook-Pro.local>
2022-06-14 11:30:38 -0500

    Acheive AUC of .87

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn import metrics
-from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,auc
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,roc_auc_score
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
@@ -15,6 +15,10 @@ from sklearn import neighbors
 
 valid = pd.read_csv("test.csv")
 base = pd.read_csv("train.csv")
+
+for col in base.columns:
+    if base[col].isnull().mean()*100>40:
+        base.drop(col,axis=1,inplace=True)
 
 f = lambda x: x.median() if np.issubdtype(x.dtype, np.number) else x.mode().iloc[0]
 base = base.fillna(base.groupby('SibSp').transform(f))
@@ -28,19 +32,19 @@ for col in base.columns:
 y = base['Survived']
 X = base[['PassengerId','Pclass','Sex','Age','SibSp','Parch','Fare','Embarked']]
 
-for col in X.columns:
-    if X[col].isnull().mean()*100>40:
-        X.drop(col,axis=1,inplace=True)
-
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20,random_state=42)
 
 def train_models(X_train, y_train):
     
  #use Decision Tree
-   
-    tree = DecisionTreeRegressor(max_features=75,max_depth=4, random_state = 0)
-    tree.fit(X_train, y_train)
-    y_pred_tree = tree.predict(X_test)
+    depth_levels = [2,3,4,5]
+    for depth in depth_levels:
+        tree = DecisionTreeRegressor(max_depth=depth, random_state = 0)
+        tree.fit(X_train, y_train)
+        y_pred_tree = tree.predict(X_test)
+        AUC_tree = roc_auc_score(y_test, y_pred_tree)
+        print(AUC_tree)
+    
 
   #use the RandomForestRegressor
     
@@ -68,21 +72,25 @@ def train_models(X_train, y_train):
     meanAbErr_tree= metrics.mean_absolute_error(y_test, y_pred_tree)
     meanSqErr_tree= metrics.mean_squared_error(y_test, y_pred_tree)
     rootMeanSqErr_tree= np.sqrt(metrics.mean_squared_error(y_test, y_pred_tree))
+    AUC_tree = roc_auc_score(y_test, y_pred_tree)
 
   # metrics of random forest regressor
     meanAbErr_rf= metrics.mean_absolute_error(y_test, y_pred_rf)
     meanSqErr_rf= metrics.mean_squared_error(y_test, y_pred_rf)
     rootMeanSqErr_rf= np.sqrt(metrics.mean_squared_error(y_test, y_pred_rf))
+    AUC_rf = roc_auc_score(y_test, y_pred_rf)
 
     # metrics of knn regressor
     meanAbErr_knn = metrics.mean_absolute_error(y_test, y_pred_knn)
     meanSqErr_knn = metrics.mean_squared_error(y_test, y_pred_knn)
-    rootMeanSqErr_knn= np.sqrt(metrics.mean_squared_error(y_test, y_pred_knn)) 
+    rootMeanSqErr_knn= np.sqrt(metrics.mean_squared_error(y_test, y_pred_knn))
+    AUC_knn = roc_auc_score(y_test, y_pred_knn) 
 
   # metrics of svr regressor
     meanAbErr_svr = metrics.mean_absolute_error(y_test, y_pred_svr_linear)
     meanSqErr_svr = metrics.mean_squared_error(y_test, y_pred_svr_linear)
-    rootMeanSqErr_svr= np.sqrt(metrics.mean_squared_error(y_test, y_pred_svr_linear)) 
+    rootMeanSqErr_svr= np.sqrt(metrics.mean_squared_error(y_test, y_pred_svr_linear))
+    AUC_svr = roc_auc_score(y_test, y_pred_svr_linear) 
 
   #print the training accurancy of each model:
 
@@ -90,11 +98,13 @@ def train_models(X_train, y_train):
     print('Mean Absolute Error:', meanAbErr_tree)
     print('Mean Square Error:', meanSqErr_tree)
     print('Root Mean Square Error:', rootMeanSqErr_tree)
+    print('AUC:', AUC_tree)
     print('\t')
     print('[2]RandomForestRegressor Training Accurancy: ',r2_score(y_test,y_pred_rf))
     print('Mean Absolute Error:', meanAbErr_rf)
     print('Mean Square Error:', meanSqErr_rf)
     print('Root Mean Square Error:', rootMeanSqErr_rf)
+    print('AUC:', AUC_rf)
     print('\t')    
     print('[3]SupportvectorRegression Accuracy(rbf): ', r2_score(y_test,y_pred_svr))
     print('\t')
@@ -102,11 +112,13 @@ def train_models(X_train, y_train):
     print('Mean Absolute Error:', meanAbErr_svr)
     print('Mean Square Error:', meanSqErr_svr)
     print('Root Mean Square Error:', rootMeanSqErr_svr)
+    print('AUC:', AUC_svr)
     print('\t')
     print('[5]knn Training Accurancy: ', r2_score(y_test,y_pred_knn))
     print('Mean Absolute Error:', meanAbErr_knn)
     print('Mean Square Error:', meanSqErr_knn)
     print('Root Mean Square Error:', rootMeanSqErr_knn)
+    print('AUC:', AUC_knn)
     print('\t')
 
 train_models(X_train,y_train)
